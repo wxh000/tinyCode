b29ae80dd7c1956d1db5ca28d4a084176f6582af
wxh47227 <wxh47227@ly.com>
2019-04-04 09:10:16 +0800

    heartsutra.c commit

---
+++ HeartSutra.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+int main()
+{
+
+    int i;   
+
+	while (1)	
+    {
+		for(i =0;i<100;i++)
+		{    
+			printf("                          bō rě bō luó mì duō xīn jīng\n");
+			printf("                           摩诃般若波罗蜜多心经\n\n\n");
+			sleep(5);
+			printf("              guān zì zài pú sà， xíng shēn bō rě bō luó mì duō shí，zhào jiàn wǔ yùn jiē kōng\n");
+			printf("              观 自 在 菩 萨， 行 深 般 若波 罗 蜜 多 时， 照见 五蕴 皆空\n\n");
+			sleep(2);
+			printf("              dù yī qiē kǔ è 。 shè lì zǐ， sè bú yì kōng ，kōng bú yì sè\n");
+			printf("              度一 切苦 厄。舍利 子，色不 异空， 空不 异色\n\n");
+			sleep(2);
+			printf("              shè lì zǐ， shì zhū fǎ kōng xiāng， bù shēng bú miè， bú gòu bú jìng\n");
+			printf("              舍 利子， 是诸 法空 相， 不生 不灭， 不垢 不净\n\n");
+			sleep(2);
+			printf("              bù zēng bù jiǎn， shì gù kōng zhōng wú sè， wú shòu xiǎng xíng shí， wú yǎn ěr\n");
+			printf("              不增 不减， 是故 空中 无色， 无受 想行 识， 无眼 耳\n\n");
+			sleep(2);
+			printf("              bí shé shēn yì， wú sè shēng xiāng wèi chù fǎ， wú yǎn jiè， nǎi zhì wú yì shí jiè\n");
+			printf("              鼻舌 身意， 无色 声香 味触 法， 无眼 界，乃 至无 意识 界\n\n");
+			sleep(2);
+			printf("              wú wú míng， yì wú wú míng jìn， nǎi zhì wú lǎo sǐ， yì wú lǎo sǐ jìn\n");
+			printf("              无无 明， 亦无 无明 尽， 乃至 无老 死，亦无 老死 尽\n\n");
+			sleep(2);
+			printf("              wú kǔ jí miè dào， wú zhì yì wú dé，yǐ wú suǒ dé gù\n");
+			printf("              无 苦集 灭道， 无智 亦无 得，以 无所 得故\n\n");
+			sleep(2);
+			printf("              pú tí sà duǒ， yī bō rě bō luó mì duō gù， xīn wú guà ài。 wú guà ài gù\n");
+			printf("              菩提 萨埵，依 般若 波罗 蜜多 故，心 无挂 碍。无 挂碍故\n\n");
+			sleep(2);
+			printf("              wú yǒu kǒng bù， yuǎn lí diān dǎo mèng xiǎng， jiū jìng niè pán\n");
+			printf("              无有 恐怖， 远离 颠倒 梦想， 究竟 涅槃\n\n");
+			sleep(2);
+			printf("              sān shì zhū fó， yī bō rě bō luó mì duō gù， dé ā nòu duō luó sān miǎo sān pú tí\n");
+			printf("              三世 诸佛， 依般 若波 罗蜜 多故， 得阿 耨多 罗三 藐三 菩提\n\n");
+			sleep(2);
+			printf("              gù zhī bō rě bō luó mì duō， shì dà shén zhòu， shì dà míng zhòu\n");
+			printf("              故知 般若 波罗 蜜多， 是大 神咒， 是大 明咒\n\n");
+			sleep(2);
+			printf("              shì wú shàng zhòu， shì wú děng děng zhòu。 néng chú yí qiè kǔ， zhēn shí bù xū\n");
+			printf("              是 无上 咒， 是无 等等 咒。 能除 一切 苦， 真实 不虚\n\n");
+			sleep(2);
+			printf("              gù shuō bō rě bō luó mì duō zhòu。 jí shuō zhòu yuē\n");
+			printf("              故 说般 若波 罗蜜 多咒。 即说 咒曰：\n\n");
+			sleep(2);
+			printf("              jiē dì jiē dì，bō luó jiē dì\n");
+			printf("              揭 谛揭 谛， 波罗 揭谛\n\n");
+			sleep(2);
+			printf("              bō luó sēng jiē dì， pú tí sá pó hē\n");
+			printf("              波罗 僧揭 谛，菩提萨 婆诃\n\n");
+			printf(" --------------------------------------------------------------------------------\n");
+		}
+	}
+
+    getch();
+
+}
+
